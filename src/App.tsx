0acf4750f745dff5d709e0e4541688f2b55ca92e
Felix Höhne <felix.hoehne@akelius.de>
2020-04-06 19:10:35 +0200

    added main wrapper for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Buttons from './components/Buttons'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  margin: auto;
+  width: 50%;
+  border: 2px solid #5C87FF;
+`
 
 function App() {
   return (
-    <div>
+    <Wrapper>
     <Buttons />
-    </div>
+    </Wrapper>
   );
 }
 
