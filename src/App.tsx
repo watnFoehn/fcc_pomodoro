855971a58f30ee6580f205083afe5d0602944ed3
Felix Höhne <felix.hoehne@akelius.de>
2020-04-13 20:15:17 +0200

    reverted from hook approach to class based

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { Component } from 'react';
 import Buttons from './components/Buttons'
 import Options from './components/Options'
 import Display from './components/Display'
@@ -19,131 +19,164 @@ left: 0;
 border: 2px solid #5C87FF;
 `
 
-
-let timerId:any;
-
-function App() {
+class App extends Component {
+  timerId:any = null;
   
-  const [sessionLength,setSessionLength] = useState(25);
-  const [breakLength,setBreakLength] = useState(5);
-  const [breakTimeLeft,setBreakTimeLeft] = useState(300);
-  const [timeLeft,setTimeLeft] = useState(1500);
-  const [isBreakRunning, setIsBreakRunning] = useState(false);
-  
-
-  const handleResetButton = () => {
-    setBreakLength(5);
-    setSessionLength(25);
-    setBreakTimeLeft(300);
-    setTimeLeft(1500);
-    setIsBreakRunning(false);
-    clearInterval(timerId);
-    timerId = null;
+    state = {
+      sessionLength: 25,
+      breakLength: 5,
+      timeLeft: 1500,
+      breakTimeLeft: 300,
+      isBreakRunning: false
+    }
+    
+  handleResetButton = () => {
+    this.setState({
+      sessionLength: 25,
+      breakLength: 5,
+      timeLeft: 1500,
+      breakTimeLeft: 300,
+      isBreakRunning: false
+    })
+    clearInterval(this.timerId);
+    this.timerId = 0;
     //TODO: this beep needs another type than any
     let beep:any = document.querySelector('#beep');
     beep.pause();
     beep.currentTime = 0;
   }
 
-  const handleOptionsButtons = (id:string) => {
-    if(timerId) {
+   handleOptionsButtons = (id:string) => {
+    if(this.timerId) {
       return;
     }
     switch(id) {
       case 'break-increment':
-        if(breakLength >= 60) {
+        if(this.state.breakLength >= 60) {
           return;
         }
-        setBreakLength(breakLength + 1);
-        setBreakTimeLeft((breakLength + 1) * 60);
+        this.setState(state => ({
+          breakLength: this.state.breakLength + 1,
+          breakTimeLeft: (this.state.breakLength + 1) * 60,
+        }));
         break;
       case 'break-decrement':
-        if(breakLength <= 1) {
+        if(this.state.breakLength <= 1) {
           return;
         }
-        setBreakLength(breakLength - 1);
-        setBreakTimeLeft((breakLength - 1) * 60);
+        this.setState(state => ({
+          breakLength: this.state.breakLength - 1,
+          breakTimeLeft: (this.state.breakLength - 1) * 60,
+        }));
         break;
       case 'session-increment':
-        if(sessionLength >= 60) {
+        if(this.state.sessionLength >= 60) {
           return;
         }
-        setSessionLength(sessionLength + 1);
-        setTimeLeft((sessionLength + 1) * 60);
+        this.setState(state => ({
+          sessionLength: this.state.sessionLength + 1,
+          timeLeft: (this.state.sessionLength + 1) * 60,
+        }));
         break;
       case 'session-decrement':
-        if(sessionLength <= 1) {
+        if(this.state.sessionLength <= 1) {
           return;
         }
-        setSessionLength(sessionLength - 1);
-        setTimeLeft((sessionLength - 1) * 60);
+        this.setState(state => ({
+          sessionLength: this.state.sessionLength - 1,
+          timeLeft: (this.state.sessionLength - 1) * 60,
+        }));
         break;
       default:
         break;
     }
   }
 
-  const handleStartButton = () => {
-    if(isBreakRunning) {
-      if(timerId) {
-        setBreakTimeLeft(breakTimeLeft)
-        clearInterval(timerId);
-        timerId = null;
+   handleStartButton = () => {
+    if(this.state.isBreakRunning) {
+      if(this.timerId) {
+        this.setState(state => (
+          {breakTimeLeft: this.state.breakTimeLeft}
+        ))
+        clearInterval(this.timerId);
+        this.timerId = 0;
         return;
       }
-      timerId = setInterval(() => {
-          setBreakTimeLeft(breakTimeLeft - 1 );
+      this.timerId = setInterval(() => {
+        this.setState(state => (
+          {breakTimeLeft: this.state.breakTimeLeft - 1}
+        ))
         },
         1000);
     } else {
-      if(timerId) {
-        setBreakTimeLeft(breakTimeLeft)
-        clearInterval(timerId);
-        timerId = null;
+      if(this.timerId) {
+        this.setState(state => (
+          {breakTimeLeft: this.state.breakTimeLeft}
+        ))
+        clearInterval(this.timerId);
+        this.timerId = 0;
         return;
       }
-      timerId = setInterval(() => {
-        setTimeLeft(timeLeft - 1 );
+      this.timerId = setInterval(() => {
+        this.setState(state => (
+          {timeLeft: this.state.timeLeft - 1}
+        ))
       },
       1000);
     }
   }
 
-  useEffect(() => {
-    if(timeLeft < 0) {
-      clearInterval(timerId);
-      let beep:any = document.querySelector('#beep');
-      beep.play();     
-      setTimeLeft(sessionLength * 60);
-      setIsBreakRunning(!isBreakRunning);
-      timerId = setInterval(() => {
-        setBreakTimeLeft(breakTimeLeft - 1);
+ componentDidUpdate = () => {
+  if(this.state.timeLeft < 0) {
+    clearInterval(this.timerId);
+    let beep:any = document.querySelector('#beep');
+    beep.play();
+    this.setState(state => {
+      return {
+        timeLeft: this.state.sessionLength * 60,
+        isBreakRunning: !this.state.isBreakRunning
+      }
+    });    
+    this.timerId = setInterval(() => {
+      this.setState(state => (
+        {breakTimeLeft: this.state.breakTimeLeft - 1}
+      ))
       },
       1000);
-    }
-    if(breakTimeLeft < 0) {
-      clearInterval(timerId);
-      let beep:any = document.querySelector('#beep');
-      beep.play(); 
-      setBreakTimeLeft(breakLength * 60);
-      setIsBreakRunning(!isBreakRunning);
-      timerId = setInterval(() => {
-        setTimeLeft(timeLeft - 1);
+  }
+  if(this.state.breakTimeLeft < 0) {
+    clearInterval(this.timerId);
+    let beep:any = document.querySelector('#beep');
+    beep.play();
+    this.setState(state => {
+      return {
+        breakTimeLeft: this.state.breakLength * 60,
+        isBreakRunning: !this.state.isBreakRunning
+      }
+    });
+    this.timerId = setInterval(() => {
+      this.setState(state => (
+        {timeLeft: this.state.timeLeft - 1}
+      ))
       },
       1000);
-    }
-  });
+  }
+}  
+ componentWillUnmount = () => {
+  clearInterval(this.timerId);
+}
 
-console.log(timerId);
-
+render() {
   return (
     <Wrapper>
-      <Options handleOptionsButtons={handleOptionsButtons} sessionLength={sessionLength} breakLength={breakLength} />
-      <Display timeLeft={timeLeft} breakTimeLeft={breakTimeLeft} isBreakRunning={isBreakRunning}/>
-      <Buttons handleResetButton={handleResetButton} handleStartButton={handleStartButton}/>
+      <Options handleOptionsButtons={this.handleOptionsButtons} sessionLength={this.state.sessionLength} breakLength={this.state.breakLength} />
+      <Display timeLeft={this.state.timeLeft} breakTimeLeft={this.state.breakTimeLeft} isBreakRunning={this.state.isBreakRunning}/>
+      <Buttons handleResetButton={this.handleResetButton} handleStartButton={this.handleStartButton}/>
       <audio src="tone.wav" id="beep"></audio>
     </Wrapper>
   );
 }
+}
+
 
 export default App;
--- src/utility/timeParser.tsx
+++ src/utility/timeParser.tsx
@@ -2,9 +2,9 @@ const timeParser = (time: number) => {
     let minutes, seconds;
     minutes = time / 60;
     seconds = time % 60;
+    minutes = minutes < 10 ? "0" + Math.floor(minutes) : Math.floor(minutes);
     seconds = seconds < 10 ? "0" + seconds : seconds;
-    return Math.floor(minutes) + ":" + seconds.toString();
-        
+    return minutes + ":" + seconds;
   }
 
   export default timeParser;
